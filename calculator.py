308aea5f29d8bd4d64956ff3f04a06cc4fda5fe4
mgnamdan <danny.ai.hub@gmail.com>
2026-03-24 20:21:13 -0400

    finish calc one

--- calculator.py
+++ calculator.py
@@ -19,7 +19,43 @@ def divide(numOne, numTwo):
 
 # ~~~~~ MAIN DEFINITION ~~~~~
 def main():
-    pass
+    calcOn = True
+
+    operations = {"+": add,
+                  "-": subtract,
+                  "*": multiply,
+                  "/": divide
+                  }
+
+    while calcOn:
+        print("\nEnter a number:")
+        firstNum = input(" --> ")
+
+        print("\nEnter an operation: [+, -, *, /]")
+        operation = input(" --> ").lower()
+
+        print("\nEnter another number:")
+        secondNum = input(" --> ")
+
+        result = operations[operation](firstNum, secondNum)
+
+        # if operation in ["+", "plus", "add"]:
+        #     result = add(firstNum, secondNum)
+        # elif operation in ["-", "minus", "subtract"]:
+        #     result = subtract(firstNum, secondNum)
+        # elif operation in ["*", "times", "multiply"]:
+        #     result = multiply(firstNum, secondNum)
+        # elif operation in ["/", "divided by", "divide"]:
+        #     result = divide(firstNum, secondNum)
+        # else:
+        #     result = "Invalid operation - try again!"
+
+        print(f"\nYour result is: {result}")
+
+        print("\nWould you like to perform another calculation? [(Y)es or (N)o]")
+        moreCalc = input(" --> ").lower()
+        if moreCalc in ["n", "no"]:
+            calcOn = False
 
 
 # ~~~~~ MAIN CALL ~~~~~
